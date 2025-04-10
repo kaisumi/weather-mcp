fa4b61e58d64df533636fae7f97dfba75edd9e8e
kimura <kaisumi@langue-lab.com>
2025-04-10 12:53:23 +0900

    chore: 初期設定

--- app/__init__.py
+++
@@ -1,6 +0,0 @@
-from .weather_service import WeatherService
-from .mcp_server import mcp, get_weather
-from .server import app, start_server
-from .client import get_weather as client_get_weather
-
-__version__ = "0.1.0" 
